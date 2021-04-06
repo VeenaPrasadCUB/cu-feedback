e6f68d0b669bf9d7b71b38dbac4034f5796904a2
Shruthi1111 <shruthishridharan95@gmail.com>
2021-04-06 00:38:48 -0600

    Commit based on sign out

--- static/index.js
+++ static/index.js
@@ -10,14 +10,29 @@ var username='';
     console.log("Image URL: " + profile.getImageUrl());
     console.log("Email: " + profile.getEmail());
 
-    // The ID token you need to pass to your backend:
+    if(document.getElementById('logged-name') != null){
+      document.getElementById('logged-name').innerHTML = "Hello, " + profile.getName();
+      console.log(document.getElementsByClassName('signout')[0].style);
+      document.getElementsByClassName('abcRioButton')[0].style.display = "none";
+      document.getElementsByClassName('signout')[0].style.display =  "block";
+    }
+    
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
+    if(!window.location.href.includes('kibana') && id_token != null)
+      window.location.href = 'kibana';
+    // The ID token you need to pass to your backend:
+    
+    //console.log("ID Token: " + id_token);
   }
 
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
+    console.log(auth2);
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
+    document.getElementById('logged-name').innerHTML = "Please Sign in";
+    document.getElementsByClassName('abcRioButton')[0].style.display =  "block";
+    document.getElementsByClassName('signout')[0].style.display =  "none";
+    window.location.href = '/';
   }
