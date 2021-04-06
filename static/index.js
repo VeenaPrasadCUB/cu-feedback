a35fff6b92e1f2df8fe7a6b1de44379b8ec00f4f
Shruthi1111 <shruthishridharan95@gmail.com>
2021-04-06 14:02:25 -0600

    Code cleanup

--- static/index.js
+++ static/index.js
@@ -1,38 +1,34 @@
 var googleUser = {};
-var username='';
-  function onSignIn(googleUser) {
-        // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
 
-    if(document.getElementById('logged-name') != null){
-      document.getElementById('logged-name').innerHTML = "Hello, " + profile.getName();
-      console.log(document.getElementsByClassName('signout')[0].style);
-      document.getElementsByClassName('abcRioButton')[0].style.display = "none";
-      document.getElementsByClassName('signout')[0].style.display =  "block";
-    }
-    
-    var id_token = googleUser.getAuthResponse().id_token;
-    if(!window.location.href.includes('kibana') && id_token != null)
-      window.location.href = 'kibana';
-    // The ID token you need to pass to your backend:
-    
-    //console.log("ID Token: " + id_token);
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  var id_token = googleUser.getAuthResponse().id_token;
+  /*console.log("ID: " + profile.getId()); 
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());*/
+
+  //If the user is logged in
+  if(document.getElementById('logged-name') != null){
+    document.getElementById('logged-name').innerHTML = "Hello, " + profile.getName();
+    document.getElementsByClassName('abcRioButton')[0].style.display = "none";
+    document.getElementsByClassName('signout')[0].style.display =  "block";
   }
+  
+  if(!window.location.href.includes('kibana') && id_token != null)
+    window.location.href = 'kibana';
+}
 
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    console.log(auth2);
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-    document.getElementById('logged-name').innerHTML = "Please Sign in";
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+
+  auth2.signOut().then(function () {
+    /*document.getElementById('logged-name').innerHTML = "Please Sign in";
     document.getElementsByClassName('abcRioButton')[0].style.display =  "block";
-    document.getElementsByClassName('signout')[0].style.display =  "none";
+    document.getElementsByClassName('signout')[0].style.display =  "none";*/
     window.location.href = '/';
-  }
+  });
+  
+}
