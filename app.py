f3083ae3213d680874969f3eb68e73791ff5da31
Shruthi1111 <shruthishridharan95@gmail.com>
2021-04-05 22:43:08 -0600

    Sign out enabled

--- app.py
+++ app.py
@@ -9,9 +9,5 @@ app = Flask(__name__,
 def index():
     return render_template("index.html")
 
-@app.route("/kibana")
-def kibana():
-    return render_template("kibana.html")
-
 if __name__ == "__main__":
     app.run()
--- static/index.js
+++ static/index.js
@@ -1,22 +1,24 @@
 var googleUser = {};
-  var startApp = function() {
-    gapi.load('auth2', function(){
-      auth2 = gapi.auth2.init({
-        client_id: '95669931471-nr74g9q5uksfl7h2l4ikc3d3mkrnd4qh.apps.googleusercontent.com',
-        cookiepolicy: 'single_host_origin',
-      });
-      attachSignin(document.getElementById('customBtn'));
-    });
-  };
+var username='';
+  function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
 
-  function attachSignin(element) {
-    console.log(element.id);
-    auth2.attachClickHandler(element, {},
-        function(googleUser) {
-          window.location.href = "kibana.html";
-          document.getElementById('name').innerText = "Signed in: " +
-              googleUser.getBasicProfile().getName();
-        }, function(error) {
-          alert(JSON.stringify(error, undefined, 2));
-        });
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+    window.location.href = 'kibana.html';
+  }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
   }
