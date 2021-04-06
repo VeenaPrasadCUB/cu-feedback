9e819499e212e7e75b9c1a642c72fb167141d142
Shruthi1111 <shruthishridharan95@gmail.com>
2021-04-05 23:13:59 -0600

    Sign out enabled

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ app = Flask(__name__,
 def index():
     return render_template("index.html")
 
+@app.route("/kibana")
+def index():
+    return render_template("kibana.html")
+
 if __name__ == "__main__":
     app.run()
