d3e14ec1a758e17980563a5e093d9d74f9d79589
VeenaPrasadCUB <54794077+VeenaPrasadCUB@users.noreply.github.com>
2021-04-06 20:54:43 -0600

    UI  Unit tests using jest

---
+++ templates/index.test.js
@@ -0,0 +1,32 @@
+import { fireEvent, getByText } from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect'
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+
+let dom
+let container
+describe('index.html', () => {
+  beforeEach(() => {
+    dom = new JSDOM(html, { runScripts: 'dangerously' })
+    container = dom.window.document.body
+  })
+
+  it('renders a heading element', () => {
+    expect(container.querySelector('h1')).not.toBeNull()
+  })
+
+   it('renders a title ', () => {
+      expect(getByText(container, 'CU FEEDBACK')).toBeInTheDocument()
+  })
+
+  it('renders google sign-in wrapper', () => {
+      expect(container.querySelectorAll('gSignInWrapper')).not.toBeNull()
+  })
+
+    it('renders a login element', () => {
+       expect(getByText(container, 'LOGIN:')).toBeInTheDocument()
+  })
+})
