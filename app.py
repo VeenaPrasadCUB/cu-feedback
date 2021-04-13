00a65c6ddf0e175293fce3a63974448337ee2b32
Jay Luther <james.m.luther@gmail.com>
2021-04-13 12:19:17 -0600

    add schema and db starter code

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
+import os
+import db
+
 
 app = Flask(__name__,
             static_folder='static',
             template_folder='templates')
+
+app.config.from_mapping(
+    DATABASE=os.path.join(app.instance_path, 'data')
+)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/kibana")
 def kibana():
     return render_template("kibana.html")
 
+
 if __name__ == "__main__":
+    db.init_app(app)
     app.run()
+
+
+
---
+++ schema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS ta;
+DROP TABLE IF EXISTS instructor;
+DROP TABLE IF EXISTS course;
+DROP TABLE IF EXISTS survey;
+
+CREATE TABLE ta (
+    cuid TEXT PRIMARY KEY NOT NULL,
+    lastname TEXT NOT NULL,
+    firstname TEXT NOT NULL,
+    current_course TEXT,
+    email TEXT NOT NULL
+);
+
+CREATE TABLE instructor (
+    faculty_id TEXT PRIMARY KEY NOT NULL,
+    lastname TEXT NOT NULL,
+    firstname TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+CREATE TABLE course (
+    cct TEXT PRIMARY KEY NOT NULL,
+    course_code TEXT NOT NULL,
+    term TEXT NOT NULL,
+    num_tas INTEGER,
+    instructor1_id TEXT NOT NULL,
+    instructor2_id TEXT,
+    ta01_id TEXT,
+    ta02_id TEXT,
+    ta03_id TEXT,
+    ta04_id TEXT,
+    ta05_id TEXT
+);
+
+CREATE TABLE survey (
+    ta_cct_index TEXT PRIMARY KEY NOT NULL,
+    ta_cuid TEXT NOT NULL,
+    cct TEXT NOT NULL,
+    index INTEGER NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    topic_1 TEXT,
+    response_1 INTEGER,
+    topic_2 TEXT,
+    response_2 INTEGER,
+    topic_3 TEXT,
+    response_3 INTEGER,
+    performance_rating INTEGER,
+    comment TEXT
+);
