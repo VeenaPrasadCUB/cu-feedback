1a5867ef11360e4f73df5bbac65b1c9b70d1e680
Pradyoth <pradhi96@gmail.com>
2021-05-04 12:50:08 -0600

    fixed merge conflicts and added unit-tests

--- DataAnalysis/data_analysis.py
+++ DataAnalysis/data_analysis.py
@@ -39,49 +39,40 @@ def populate_dictionary(scores_aggregate):
 
 
 def calculate_scores():
-    input_data = read_data('sample_data.csv')
-    array_scores = input_data['How comfortable are you working with arrays?']
-    pointer_scores = input_data['How comfortable are you working with pointers?']
-    linked_list_scores = input_data['How comfortable are you working with Linked Lists?']
-    ta_ratings = input_data['How would you rate your TA?']
-    recitation_text = input_data['If you could improve one thing about Recitation, what would it be?']
-    array_scores_aggregate = Counter(array_scores)
-    linked_list_scores_aggregate = Counter(linked_list_scores)
-    pointer_scores_aggregate = Counter(pointer_scores)
-    populate_dictionary(array_scores_aggregate)
-    populate_dictionary(pointer_scores_aggregate)
-    populate_dictionary(linked_list_scores_aggregate)
-    agg = ta_ratings.agg(['min', 'max', 'average'])
-    ta_max_rating = round((float)(agg['max']), 2)
-    ta_min_rating = round((float)(agg['min']), 2)
-    ta_average_rating = round((float)(agg['average']), 2)
-    ta_scores = {"ta_max_rating": ta_max_rating, "ta_min_rating": ta_min_rating, "ta_average_rating": ta_average_rating}
-    polarity_dictionary = {}
-    for text in recitation_text:
-        sentiment = compute_sentiment(text)
-        polarity_dictionary[sentiment] = polarity_dictionary.get(sentiment, 0) + 1
-
-    return [array_scores_aggregate, pointer_scores_aggregate, linked_list_scores_aggregate, ta_scores,
-            polarity_dictionary]
-
+  input_data = read_data('sample_data.csv')
+  array_scores = input_data['How comfortable are you working with arrays?']
+  pointer_scores = input_data['How comfortable are you working with pointers?']
+  linked_list_scores = input_data['How comfortable are you working with Linked Lists?']
+  ta_ratings = input_data['How would you rate your TA?']
+  recitation_text = input_data['If you could improve one thing about Recitation, what would it be?']
+  array_scores_aggregate = Counter(array_scores)
+  linked_list_scores_aggregate = Counter(linked_list_scores)
+  pointer_scores_aggregate = Counter(pointer_scores)
+  populate_dictionary(array_scores_aggregate)
+  populate_dictionary(pointer_scores_aggregate)
+  populate_dictionary(linked_list_scores_aggregate)
+  agg = ta_ratings.agg(['min', 'max', 'average'])
+  ta_max_rating = round((float) (agg['max']), 2)
+  ta_min_rating = round((float) (agg['min']), 2)
+  ta_average_rating =  round((float) (agg['average']), 2)
+  ta_scores = {"ta_max_rating" : ta_max_rating, "ta_min_rating" :ta_min_rating, "ta_average_rating" : ta_average_rating}
+  polarity_dictionary = {} 
+  for text in recitation_text:
+    sentiment = compute_sentiment(text)
+    polarity_dictionary[sentiment] = polarity_dictionary.get(sentiment, 0) + 1
+    
+  return [array_scores_aggregate, pointer_scores_aggregate, linked_list_scores_aggregate, ta_scores, polarity_dictionary]
 
 def main():
-    scores = calculate_scores()
-    array_scores_aggregate, pointer_scores_aggregate, linked_list_scores_aggregate, ta_scores, polarity_dictionary = \
-    scores[0], scores[1], scores[2], scores[3], scores[4]
-    print('array scores {}'.format(array_scores_aggregate))
-    print('pointer scores {}'.format(pointer_scores_aggregate))
-    print('linked list scores {}'.format(linked_list_scores_aggregate))
-    print('TA Scores {}'.format(ta_scores))
-    print('Polarity Dictionary {}'.format(polarity_dictionary))
-    return array_scores_aggregate, pointer_scores_aggregate, linked_list_scores_aggregate, ta_scores, polarity_dictionary
-
+  scores = calculate_scores()
+  array_scores_aggregate, pointer_scores_aggregate, linked_list_scores_aggregate, ta_scores, polarity_dictionary = scores[0], scores[1], scores[2], scores[3], scores[4]
+  return array_scores_aggregate, pointer_scores_aggregate, linked_list_scores_aggregate, ta_scores, polarity_dictionary
 
 def get_es_credentials():
-    return {
-        "user": "elastic",
-        "password": "s5B0DDHP35MTfvYLvnPx1afX",
-        "cloud_id": "fos-project:dXMtd2VzdDEuZ2NwLmNsb3VkLmVzLmlvJGYwYjZlYzVhYzlkNTQ3ZmY5NWQ3Yjg0YmNlN2Q5MDM1JDFhNWMyNzBlYzk1NjQwOGQ5ZGM4MTUxMjMyNDdhMWM0"
+  return {
+      "user": "elastic", 
+      "password": "t09VS1JgcawQAfqgTmJI0AwY",
+      "cloud_id": "elastic:dXMtd2VzdDEuZ2NwLmNsb3VkLmVzLmlvJDViYTJlMTQwNjkzZDQ1ZTJiMDVmNzNiYjIwOWNjNGQ3JDY3ODIxMWU3NWZhYjRlZjQ5ZWRiMTRlYmQxOTJiMzgx"
     }
 
 
@@ -89,30 +80,25 @@ ELASTIC_SETTINGS = get_es_credentials()
 
 
 def es(array_scores_aggregate, pointer_scores_aggregate, linked_list_scores_aggregate, ta_scores, polarity_dictionary):
-    es = Elasticsearch(
-        cloud_id=ELASTIC_SETTINGS["cloud_id"],
-        http_auth=(ELASTIC_SETTINGS["user"], ELASTIC_SETTINGS["password"]),
-    )
+  es = Elasticsearch(
+      cloud_id=ELASTIC_SETTINGS["cloud_id"],
+      http_auth=(ELASTIC_SETTINGS["user"], ELASTIC_SETTINGS["password"]),
+  )
+  
+  #ta_scores = {"ta_max_rating" : ta_max_rating, "ta_min_rating" :ta_min_rating, "ta_average_rating" : ta_average_rating}
+  es.index(index='sw', doc_type='people', id=1, body= array_scores_aggregate)
+  es.index(index='sw', doc_type='people', id=2, body= pointer_scores_aggregate)
+  es.index(index='sw', doc_type='people', id=3, body= linked_list_scores_aggregate)
+  es.index(index='sw', doc_type='people', id=4, body= ta_scores)
+  es.index(index='sw', doc_type='people', id=5, body= polarity_dictionary)
 
-    # ta_scores = {"ta_max_rating" : ta_max_rating, "ta_min_rating" :ta_min_rating, "ta_average_rating" : ta_average_rating}
-    es.index(index='sw', doc_type='people', id=1, body=array_scores_aggregate)
-    es.index(index='sw', doc_type='people', id=2, body=pointer_scores_aggregate)
-    es.index(index='sw', doc_type='people', id=3, body=linked_list_scores_aggregate)
-    es.index(index='sw', doc_type='people', id=4, body=ta_scores)
-    es.index(index='sw', doc_type='people', id=5, body=polarity_dictionary)
+  array_scores_1 = es.get(index='sw', doc_type='people', id=1)
+  pointer_scores_1 = es.get(index='sw', doc_type='people', id=2)
+  linked_list_scores_1 = es.get(index='sw', doc_type='people', id=3)
+  ta_scores_1 = es.get(index='sw', doc_type='people', id=4)
+  polarity_dictionary_1 = es.get(index='sw', doc_type='people', id=5)
 
-    array_scores_1 = es.get(index='sw', doc_type='people', id=1)
-    pointer_scores_1 = es.get(index='sw', doc_type='people', id=2)
-    linked_list_scores_1 = es.get(index='sw', doc_type='people', id=3)
-    ta_scores_1 = es.get(index='sw', doc_type='people', id=4)
-    polarity_dictionary_1 = es.get(index='sw', doc_type='people', id=5)
-
-    print(array_scores_1)
-    print(pointer_scores_1)
-    print(linked_list_scores_1)
-    print(ta_scores_1)
-    print(polarity_dictionary_1)
-
+  return [array_scores_1, pointer_scores_1, linked_list_scores_1, ta_scores_1, polarity_dictionary_1 ]
 
 array_scores_aggregate, pointer_scores_aggregate, linked_list_scores_aggregate, ta_scores, polarity_dictionary = main()
 print()
--- DataAnalysis/test_data_analysis.py
+++ DataAnalysis/test_data_analysis.py
@@ -5,10 +5,65 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import unittest
 from data_analysis import *
+from unittest.mock import patch
+from collections import Counter
+import pandas as pd
+import numpy as np
+
+
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual(compute_sentiment('Hi example test'), 'positive')
+    # unit test for testing compute_sentiment
+    def test_compute_sentiment(self):
+        self.assertEqual(compute_sentiment('Hi example test'), 'neutral')
+        self.assertEqual(compute_sentiment('Good'), 'positive')
+        self.assertEqual(compute_sentiment('Bad'), 'negative')
+    
+    #acceptance test for data_analysis file
+    def test_calculate_scores(self):
+        scores_list = calculate_scores()
+        expected_results = [Counter({5: 14, 4: 8, 3: 6, 1: 0, 2: 0}),
+                            Counter({4: 15, 3: 7, 5: 5, 1: 1, 2: 0}),
+                            Counter({3: 10, 4: 10, 5: 5, 2: 2, 1: 1}),
+                            {'ta_average_rating': 4.68, 'ta_max_rating': 5.0, 'ta_min_rating': 3.0},
+                            {'negative': 1, 'neutral': 15, 'positive': 12}]
+        self.assertEqual(len(scores_list), 5)
+        self.assertNotEqual(len(scores_list), 10)
+        self.assertEqual(scores_list, expected_results)
+    
+    # Unit test to demonstrate mocking of read_data function present in the data_analysis module. 
+    
+    @patch('data_analysis.read_data')
+    def test_calculate_scores_using_mock(self, mocked_obj):
+        
+        data = pd.DataFrame()
+        num_responses = 10
+        sample_comments = ['no comment, good job','more examples please','slow down, we cannot keep up',
+                   'na','none','',
+                   'slower!','share your slides please','you\'re doing great!']
+        response_values = [1]
+        data['Timestamp'] = np.random.choice(['', ''], num_responses)
+        data['How comfortable are you working with arrays?'] = np.random.choice(response_values, num_responses)
+        data['How comfortable are you working with pointers?'] = np.random.choice(response_values, num_responses)
+        data['How comfortable are you working with Linked Lists?'] = np.random.choice(response_values, num_responses)
+        data['How would you rate your TA?'] = np.random.choice(response_values, num_responses)
+        data['If you could improve one thing about Recitation, what would it be?'] = np.random.choice(sample_comments, num_responses)
+        mocked_obj.return_value = data
+        input_data = pd.read_csv('sample_data.csv')
+        input_data = input_data.fillna('.')
+        self.assertEqual(read_data('sample_data.csv')['How would you rate your TA?'][0], input_data['How would you rate your TA?'][0])
 
+
+    #integration test to check read_data and populate_dictionary modules are integrated correctly.
+    def test_integration(self):
+        input_data = read_data('sample_data.csv')
+        array_scores = input_data['How comfortable are you working with arrays?']
+        array_scores_aggregate = Counter(array_scores)
+        expected_array_scores_aggregate = Counter({5: 14, 4: 8, 3: 6})
+        self.assertEqual(expected_array_scores_aggregate, array_scores_aggregate)
+        actual_results = populate_dictionary(array_scores_aggregate)
+        expected_results = None
+        self.assertEqual(actual_results, expected_results)
+    
 if __name__ == '__main__':
     unittest.main()
